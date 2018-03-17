27d417766ebdab1a9a77dd164ea6a3b871fbe84c
ryanzam <ryanzam2005@yahoo.com>
2018-03-17 09:27:18 +0200

    mean app

---
+++ app.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
+var favi = require('serve-favicon');
+var logger = require('morgan');
+
+var books = require("./routes/book");
+var index = require("./routes/index");
+
+var port = 3000;
+
+var app = express();
+
+//middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({'extended': 'false'}));
+app.use(logger('dev'));
+
+//static folder "client" 
+app.use(express.static(path.join(__dirname, 'client')));
+
+app.use('/', index)
+app.use('/api', books)
+
+//start server
+
+app.listen(port, function(){
+    console.log("Server started : " + port)
+});
